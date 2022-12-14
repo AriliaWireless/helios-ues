bff7b2e23e0f98af4d4d758503c482f3544ccffe
stephb9959 <stephane.bourque@gmail.com>
2022-12-14 11:58:29 -0800

    Initial commit

--- src/RESTAPI/RESTAPI_ue_handler.cpp
+++ src/RESTAPI/RESTAPI_ue_handler.cpp
@@ -3,11 +3,21 @@
 //
 
 #include "RESTAPI_ue_handler.h"
+#include "framework/utils.h"
+#include "StorageService.h"
+#include "ar_RESTObjects/RESTAPI_LOCObjects.h"
 
 namespace Arilia {
 
     void RESTAPI_ue_handler::DoGet() {
-
+        auto ue = GetBinding("ue","");
+        if(!OpenWifi::Utils::NormalizeMac(ue)) {
+            return BadRequest(OpenWifi::RESTAPI::Errors::InvalidSerialNumber);
+        }
+        LOCObjects::UEEntry UE;
+        if(OpenWifi::StorageService()->UELocatorDB().GetRecord("ue",ue,UE)) {
+            return OpenWifi::RESTAPIHandler::Object(UE);
+        }
     }
 
 } // Arilia
