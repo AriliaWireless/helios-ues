aea6122115c9c10330e1a988822b97e53bb857e3
stephb9959 <stephane.bourque@gmail.com>
2023-01-10 21:53:57 -0800

    Initial commit

--- src/UELocatorWebSocketClient.h
+++ src/UELocatorWebSocketClient.h
@@ -11,7 +11,7 @@ namespace OpenWifi {
 	  public:
 		explicit UELocatorWebSocketClient(Poco::Logger &Logger);
 		virtual ~UELocatorWebSocketClient();
-		virtual void Processor(const Poco::JSON::Object::Ptr &O, std::string &Answer, bool &Done );
+		virtual void Processor(const Poco::JSON::Object::Ptr &O, std::string &Answer, bool &Done, const SecurityObjects::UserInfo &UserInfo );
 		void ws_command_serial_number_search( const Poco::JSON::Object::Ptr &O, bool &Done, std::string &Answer);
 		void ws_command_exit( const Poco::JSON::Object::Ptr &O, bool &Done, std::string &Answer);
 		void ws_command_invalid( const Poco::JSON::Object::Ptr &O, bool &Done, std::string &Answer);
